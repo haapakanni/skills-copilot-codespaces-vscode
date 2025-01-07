d403d7b065422d0ff8a5981e8ac45056f073f3df
Henri Haapakanni <39362660+haapakanni@users.noreply.github.com>
2025-01-07 10:38:57 +0000

    Third commit

--- skills.js
+++ skills.js
@@ -13,3 +13,12 @@ displaySkills(skills);
 function calculateNumbers(var1, var2) {
     return var1 + var2;
 }
+
+//multiply numbers
+function multiplyNumbers(var1, var2) {
+    return var1 * var2;
+}
+
+function calculateNubmersAgain(var1, var2) {
+    return var1 - var2;
+}
